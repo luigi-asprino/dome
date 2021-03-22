080b19468116f9c8d53b335c6d90262ee9e8d175
Luigi Asprino <luigi.asprino@gmail.com>
2021-03-22 23:20:25 +0100

    tim properties

--- src/main/java/it/cnr/istc/stlab/CategoryHierarchyAnalyser.java
+++ src/main/java/it/cnr/istc/stlab/CategoryHierarchyAnalyser.java
@@ -8,7 +8,7 @@ import it.cnr.istc.stlab.edwin.RocksDBBackedEquivalenceSetGraph;
 public class CategoryHierarchyAnalyser {
 
 	public static void main(String[] args) throws IOException {
-		RocksDBBackedEquivalenceSetGraph esg = Edwin.computeESG("src/main/resources/dbc.properties");
+		RocksDBBackedEquivalenceSetGraph esg = Edwin.computeESG("src/main/resources/dbc_tim.properties");
 		esg.getStats().toTSVFile("src/main/resources/stats.tsv");
 	}
 
